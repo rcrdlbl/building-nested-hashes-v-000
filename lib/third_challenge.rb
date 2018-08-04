3cb0875f00b6cb251ad59cd330ab708b92c98ad1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-04 22:51:49 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         name: "Romeo",
-        age: "14",
+        age: "15",
         status: "alive",
         },
       :hero_friends => []
